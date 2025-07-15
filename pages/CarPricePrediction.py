f4c66a03185239ef3c9dff2a9f652bd0a9ae0f4c
Mohamed Hesham <mohamedheshamrg@gmail.com>
2025-07-15 05:18:27 +0300

    Update CarPricePrediction.py

--- pages/CarPricePrediction.py
+++ pages/CarPricePrediction.py
@@ -3,8 +3,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from frequency_encoder import FrequencyEncoder 
-# تحميل ملف frequency_encoder.py من GitHub
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class FrequencyEncoder(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        self.freq_maps = {
+            col: X[col].value_counts(normalize=True)
+            for col in X.columns
+        }
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for col in X.columns:
+            X[col] = X[col].map(self.freq_maps[col]).fillna(0)
+        return X
+
 
 
 pipeline = joblib.load("car_price_stacking_ML_model.pkl")
