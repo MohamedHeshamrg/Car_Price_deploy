a53607675af6a26ab0555e59bf9da225191d7cb1
Mohamed Hesham <mohamedheshamrg@gmail.com>
2025-07-16 02:28:08 +0300

    Update CarPricePrediction.py

--- pages/CarPricePrediction.py
+++ pages/CarPricePrediction.py
@@ -4,20 +4,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.base import BaseEstimator, TransformerMixin
-class FrequencyEncoder(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        self.freq_maps = {
-            col: X[col].value_counts(normalize=True)
-            for col in X.columns
-        }
-        return self
-
-    def transform(self, X):
-        X = X.copy()
-        for col in X.columns:
-            X[col] = X[col].map(self.freq_maps[col]).fillna(0)
-        return X
-
+from frequency_encoder import FrequencyEncoder
 
 @st.cache_data
 def load_data():
